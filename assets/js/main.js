b5b76620895112a5ebffa678d09c764a5d42ef3c
Szabo Laszlo <szabo.laszlo@szawary.eu>
2020-12-12 20:25:33 +0100

    part 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,8 @@
         'fa-cab',
     ];
 
+    let points = 0;
+
     /*
         <div class="col-2 offset-1">
              <div class="front">
@@ -69,13 +71,51 @@
             row2.appendChild(card);
         }
     }
-
+    let blockClicks = false;
     const cardClick = (ev) => {
+        if (blockClicks) {
+            return;
+        }
+
         ev.currentTarget.classList.toggle('flipped');
+        const flippedCard = document.querySelectorAll('.card.flipped');
+        if (flippedCard.length > 1) {
+            blockClicks = true;
+            const to = setTimeout(() => {
+                clearTimeout(to);
+                blockClicks = false;
+                document.querySelectorAll('.card').forEach(card => {
+                    card.classList.remove('flipped');
+                });
+            }, 2000);
+
+            checkPair();
+        }
+
+
     };
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => {
         card.addEventListener('click', cardClick);
     })
+
+    const showPoints = (points) => {
+        document.querySelector('user-points').textContent = points;
+    }
+
+    const checkPair = () => {
+        const firstCardIcon = document.querySelector('.card.flipped i');
+        if (firstCardIcon) {
+            const firstIconClass = firstCardIcon.className.split(' ');
+            const pair = document.querySelectorAll(`card.flipped .${firstIconClass.pop()}`);
+            if (pair.length == 2) {
+                points++;
+                showPoints(points);
+                document.querySelectorAll(`.card.flipped`).forEach(
+                    card => card.classList.add('found')
+                );
+            }
+        }
+    }
 
 })();
